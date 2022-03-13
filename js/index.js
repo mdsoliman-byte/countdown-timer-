dab828a336628a299b5cf6f6f78e52a523e1211b
mdsoliman21 <89044891+mdsoliman-me@users.noreply.github.com>
2022-03-14 03:13:37 +0600

    added some function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-  
+  const newYears = "4 Apr 2022";
+function countDown() {
+  const newYearsDate = new Date(newYears);
+  const courentDate = new Date();
+  const totalSecound = (newYearsDate - courentDate) / 1000;
+  const days = Math.floor(totalSecound / 3600 / 24);
+  getUiDAta("dayes", days);
+  const howesr = Math.floor((totalSecound / 3600) % 24);
+  getUiDAta("hoowurs", howesr);
+  const min = Math.floor((totalSecound / 60) % 60);
+  getUiDAta("munites", min);
+
+  const sec = Math.floor(totalSecound % 60);
+  getUiDAta("secound", sec);
+  console.log(days, howesr, min, sec);
+}
+function getUiDAta(userInput, val) {
+  const uiId = document.getElementById(userInput);
+  uiId.innerText = val;
+  return uiId;
+}
+countDown();
+
+setInterval(countDown, 1000);
